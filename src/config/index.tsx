e2775625089bbbfdf6ba30f6aba9bda63113e476
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-07-16 09:00:06 +0800

    update layout ConnectButton and config

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 
 import { cookieStorage, createStorage } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { holesky, mainnet, polygon, sepolia } from 'wagmi/chains'
 
 // Get projectId from https://cloud.walletconnect.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
@@ -16,7 +16,7 @@ const metadata = {
 }
 
 // Create wagmiConfig
-const chains = [mainnet, sepolia] as const
+const chains = [mainnet, sepolia, holesky, polygon] as const
 export const config = defaultWagmiConfig({
     chains,
     // transports: {
@@ -24,6 +24,12 @@ export const config = defaultWagmiConfig({
     // },
     projectId,
     metadata,
+    auth: {
+        email: true, // default to true
+        socials: ['google', 'x', 'github', 'discord', 'apple'],
+        showWallets: true, // default to true
+        walletFeatures: true // default to true
+    },
     ssr: true,
     storage: createStorage({
         storage: cookieStorage
